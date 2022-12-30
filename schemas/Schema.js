f14cc7cf1dda548c721738dd02003b0212a054ed
Tumax89 <yourgitemail@example.com>
2022-12-30 15:34:26 +0800

    Scheme дээр бага зэрэг засвар оруулав.

--- schemas/Schema.js
+++ schemas/Schema.js
@@ -1,39 +1,41 @@
-export const schemaTypes = {
-  title: 'Постууд',
-  name: 'post',
-  type: 'document',
-  fields: [
-    {
-      title: 'Постын гарчиг',
-      name: 'title',
-      type: 'string',
-      description: 'Блогийн постын үндсэн гарчиг, 50 тэмдэгтээс бүү хэтрүүлээрэй',
-    },
-    {
-      title: 'Дэд гарчиг',
-      name: 'subtitle',
-      type: 'string',
-      description: 'Постын төрлийг текстээр бичих',
-    },
-    {
-      title: 'Постын зураг',
-      name: 'cover_image',
-      type: 'image',
-    },
-    {
-      title: 'Огноо',
-      name: 'date',
-      type: 'datetime',
-    },
-    {
-      title: 'Хаяг',
-      name: 'slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-
-        slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+export const schemaTypes = [
+  {
+    title: 'Постууд',
+    name: 'post',
+    type: 'document',
+    fields: [
+      {
+        title: 'Постын гарчиг',
+        name: 'title',
+        type: 'string',
+        description: 'Блогийн постын үндсэн гарчиг, 50 тэмдэгтээс бүү хэтрүүлээрэй',
       },
-    },
-  ],
-}
+      {
+        title: 'Дэд гарчиг',
+        name: 'subtitle',
+        type: 'string',
+        description: 'Постын төрлийг текстээр бичих',
+      },
+      {
+        title: 'Постын зураг',
+        name: 'cover_image',
+        type: 'image',
+      },
+      {
+        title: 'Огноо',
+        name: 'date',
+        type: 'datetime',
+      },
+      {
+        title: 'Хаяг',
+        name: 'slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+
+          slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+        },
+      },
+    ],
+  },
+]
